7c487c58151c52d4131c17aaf0435e9d1f044ce0
Michael Canon <69116412+mjcanon4@users.noreply.github.com>
2021-02-12 00:52:09 -0500

    creating the auth, the loader, the api set up, the env, set up axios, config for spotify dev

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
   return (
     <React.Fragment>
       <Header />
-      Page not found. Goto <Link to="/dashboard">Home Page</Link>
+      Page not found. Go to <Link to="/dashboard">Home Page</Link>
     </React.Fragment>
   );
 };
